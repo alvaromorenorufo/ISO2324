4e15817ad6b4ad24f2806dd41207b9f24cbfb597
alvaromorenorufo <150673446+alvaromorenorufo@users.noreply.github.com>
2023-11-17 10:34:16 +0100

    directories.bat

--- procesoslotes/directories.bat
+++ procesoslotes/directories.bat
@@ -3,14 +3,37 @@ REM Crear menu
 REM
 REM Autor: Alvaro Moreno
 REM Fecha: 15-11-2023
+echo 1- Crear fichero
+echo 2- Mostrar arbol directorios
+echo 3- Mostrar archivos .txt
+echo 4- Crea los directorios alfredoff marinapg ramonam
+echo 5- Copia contenido a la carpeta copia
 
+set/p numero=" Introduce la opcion que deseas "
+if %numero%== 1 goto numero1
+if %numero%== 2 goto numero2
+if %numero%== 3 goto numero3
+if %numero%== 4 goto numero4
+if %numero%== 5 goto numero5
+
+:numero1
 set/p fichero="Introduce el nombre de fichero "
 echo > %fichero%.txt se ha creado con exito
-echo.
+exit
+
+:numero2
 tree %USERPROFILE%
-echo.
+pause
+exit
+
+:numero3
 dir *.txt
-echo.
+pause
+exit
+:numero4
 mkdir alfredoff marinapg ramonam
-echo. 
+exit
+
+:numero5
 xcopy "C:\Users\alumno\Desktop\scripts" "C:\copia" 
+exit
